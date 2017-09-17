c5e3f404b3d0b6d282176b07d500400dcd36ce57
GokuDBZ <suraj.yadav1994@gmail.com>
2017-09-17 02:03:30 +0000

    added routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :lifts
   resources :test_articles
+  resource :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
