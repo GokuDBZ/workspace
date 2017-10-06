c8f9954bb848f586843da1eb22dc64b627162656
GokuDBZ <suraj.yadav1994@gmail.com>
2017-10-06 02:42:26 +0000

    added migration for association / bw user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    has_many :comments
+    before_save {self.email = self.email.downcase}
+    has_many :articles
     validates :user_name, length: {minimum: 3,maximum: 50}, uniqueness: {case_sensitive: false}, presence: true
     validates :email, length:{maximum: 50}, uniqueness: true, presence: true , 
                 format: {with: VALID_EMAIL_REGEX, message: "email is not valid"}
