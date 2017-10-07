d6403fe4ae8606257ef3e681f8d637ac7230271e
GokuDBZ <suraj.yadav1994@gmail.com>
2017-10-07 16:29:41 +0000

    model chanes for user~

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     before_save {self.email = self.email.downcase}
+    
+    
     has_many :articles
     validates :user_name, length: {minimum: 3,maximum: 50}, uniqueness: {case_sensitive: false}, presence: true
     validates :email, length:{maximum: 50}, uniqueness: true, presence: true , 
